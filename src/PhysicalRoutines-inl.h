0438bdb563451092211467928c31ff1092681eab
Robert Obryk <robryk@gmail.com>
2012-02-29 19:47:55 +0100

    New physical routines: max_relativistic_energy_transfer and dependencies

--- src/PhysicalRoutines-inl.h
+++ src/PhysicalRoutines-inl.h
@@ -10,7 +10,31 @@ namespace physical {
 inline Real beta_from_E(Real E_MeV_u)
 {
 	assert(E_MeV_u >= 0.0);
-	return sqrt(1.0 - 1.0/square(1.0 + E_MeV_u/atomic_mass_unit_MeV_c2));
+	Real beta = sqrt(1.0 - 1.0/square(1.0 + E_MeV_u/atomic_mass_unit_MeV_c2));
+	assert(beta < 1.0);
+	return beta;
+}
+
+inline Real gamma_from_E(Real E_MeV_u)
+{
+	assert(E_MeV_u >= 0.0);
+	return 1.0 / sqrt(1.0 - square(beta_from_E(E_MeV_u)));
+}
+
+// TODO(robryk): Is the argument mislabeled? This gets passed a beta value.
+inline Real mass_correction_terms(Real E_MeV_u)
+{
+	Real gamma = gamma_from_E(E_MeV_u);
+	Real m_MeV_c2 = 1.0079 * proton_mass_MeV_c2; // TODO(original): what does it mean MeV_c2, are units correct ?
+    return 1.0 + (2.0 * (electron_mass_MeV_c2 / m_MeV_c2) / gamma) + square(electron_mass_MeV_c2 / m_MeV_c2);
+}
+
+inline Real max_relativistic_energy_transfer(Real E_MeV_u)
+{
+	assert(E_MeV_u >= 0.0);
+	Real beta = beta_from_E(E_MeV_u);
+	Real mass_correction_terms = mass_correction_terms(E_MeV_u);
+	return (2.0 * electron_mass_MeV_c2 * square(beta) / (1.0 - square(beta))) / mass_correction_terms;
 }
 
 }
--- src/PhysicalRoutines.h
+++ src/PhysicalRoutines.h
@@ -6,13 +6,28 @@
 namespace amtrack {
 namespace physical {
 
-	/* P.J. Mohr and D.B. Newell,
+	/* 
+	 * Atomic mass.
+	 * 
+	 * P.J. Mohr and D.B. Newell,
 	 * "Resource Letter FC-1: The Physics of Fundamental Constants,"
 	 * Am. J. Phys, 78 (2010) 338
 	 */
 const double atomic_mass_unit_MeV_c2 = 931.494028;
+	/*
+	 * Proton mass.
+	 * 
+	 * http://physics.nist.gov/constants
+	 */
+const double proton_mass_MeV_c2 = 938.272013;
+
 
 inline Real beta_from_E(Real E_MeV_u);
+inline Real gamma_from_E(Real E_MeV_u);
+// TODO(robryk): Description
+inline Real mass_correction_terms(Real E_MeV_u);
+// TODO(robryk): Description
+inline Real max_relativistic_energy_transfer(Real E_MeV_u);
 
 }
 }
